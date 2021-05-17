0dbaad3ae588cf337808581f5f1435dece5c1ad9
Piotr <piotr.firebrigade@gmail.com>
2021-05-17 12:50:07 +0200

    Update hello.scala
    end zad 1

--- hello.scala
+++ hello.scala
@@ -1,24 +1,56 @@
 object HelloWord {
 
-    def main(args: Array[String]){
-       var listDay = createListDayWeek()
+  def main(args: Array[String]) {
+    //    zad1a
+    //    zad1b
+    //    zad1c
+  }
 
-       var returnString : String = ""
-        returnString.concat("v")
+  def zad1b: Unit = {
+    //        b. Pętli for wypisując tylko dni z nazwami zaczynającymi się na „P” - zmieniłem na T gdyz po ang. nie ma P :)
+    var listDay = createListDayWeek()
 
-       for(day <- listDay){
-           returnString =  returnString.concat(day + ",")
-       }     
-       
-       println(returnString.dropRight(1))
-    } 
+    var returnString: String = ""
 
-
-        
-    def createListDayWeek() : List[String] = {
-       var dayWeek: List[String] = List("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "SunDay")
-        
-
-       return dayWeek
+    for (day <- listDay if day.startsWith("T")) {
+      returnString = returnString.concat(day + ",")
     }
+
+    println(returnString.dropRight(1))
+  }
+
+  def zad1c: Unit = {
+    //        c. Pętli while
+    var listDay = createListDayWeek()
+    var lngth = listDay.length - 1
+
+    var returnString: String = ""
+
+    var i = 0
+    while (i <= lngth) {
+      returnString = returnString.concat(listDay(i) + ",")
+      i += 1
+    }
+
+    println(returnString.dropRight(1))
+  }
+
+  def zad1a: Unit = {
+    var listDay = createListDayWeek()
+
+    var returnString: String = ""
+
+    for (day <- listDay) {
+      returnString = returnString.concat(day + ",")
+    }
+
+    println(returnString.dropRight(1))
+  }
+
+  def createListDayWeek(): List[String] = {
+    var dayWeek: List[String] = List("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "SunDay")
+
+
+    return dayWeek
+  }
 }
